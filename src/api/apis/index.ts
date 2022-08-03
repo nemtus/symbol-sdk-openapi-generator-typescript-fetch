73691a661e6b69cee7c08f4b7848f22f313746ca
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-08-03 14:45:12 +0900

    feat: generate REST API client code with OpenAPI Generator typescript-fetch
    npm run openapi:generate

---
+++ src/api/apis/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountRoutesApi';
+export * from './BlockRoutesApi';
+export * from './ChainRoutesApi';
+export * from './FinalizationRoutesApi';
+export * from './HashLockRoutesApi';
+export * from './MetadataRoutesApi';
+export * from './MosaicRoutesApi';
+export * from './MultisigRoutesApi';
+export * from './NamespaceRoutesApi';
+export * from './NetworkRoutesApi';
+export * from './NodeRoutesApi';
+export * from './ReceiptRoutesApi';
+export * from './RestrictionAccountRoutesApi';
+export * from './RestrictionMosaicRoutesApi';
+export * from './SecretLockRoutesApi';
+export * from './TransactionRoutesApi';
+export * from './TransactionStatusRoutesApi';
